0cf0ecc89ee4e236f58eb83bfe0692ee055c383f
Stas Karpov <gilbert_90@mail.ru>
2016-05-26 22:31:25 +0300

    Limit template paths to only local assets

--- lib/templates_paths.rb
+++ lib/templates_paths.rb
@@ -4,7 +4,7 @@ module TemplatesPaths
   def templates
     Hash[
       Rails.application.assets.each_file.
-      select { |file| file.end_with?('swf', 'html', 'json') }.
+      select { |file| file.include?(Rails.root.to_s) && file.end_with?('swf', 'html', 'json') }.
       map { |file| Rails.application.assets.find_asset(file).logical_path }.
       map { |file| [file, ActionController::Base.helpers.asset_path(file)] }
     ]
