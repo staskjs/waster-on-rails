056fb0ed0ca445c3dded43d82ed628b2c7f95700
Stas Karpov <gilbert_90@mail.ru>
2016-05-27 11:38:57 +0300

    Rewrite js_env helper to properly invalidate cache

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def js_env
+    templates = Hash[
+      Rails.application.assets.each_file
+           .select { |file| file.include?(Rails.root.to_s) && file.end_with?('html', 'json') }
+           .map { |file| Rails.application.assets.find_asset(file).logical_path }
+           .map { |file| [file, asset_path(file)] }
+    ]
+
+    data = {
+      env: Rails.env,
+      templates: templates,
+      app_name: Rails.application.class.parent_name
+    }
+
+    <<-EOS.html_safe
+      <script type="text/javascript">
+        shared = angular.module('#{data[:app_name]}')
+        shared.constant('Rails', #{data.to_json})
+      </script>
+    EOS
+  end
 end
