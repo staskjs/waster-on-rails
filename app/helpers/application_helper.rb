fd11563e20d608d00a59a0b1fb35a4e697b63199
Stas Karpov <gilbert_90@mail.ru>
2016-07-04 17:11:32 +0300

    Make assets catalog separately for production env

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,24 @@
 module ApplicationHelper
   def js_env
-    assets = Hash[
-      Rails.application.assets.each_file
-        .select do |file|
-          file.include?(Rails.root.to_s) && file.end_with?('html', 'html.erb', 'json', 'json.erb')
-        end
-        .map { |file| Rails.application.assets.find_asset(file).logical_path }
-        .map { |file| [file, asset_path(file)] }
-    ]
+    if Rails.env.production?
+      assets = Hash[
+        Rails.application.assets_manifest.files
+          .select do |file|
+            file.end_with?('html', 'html.erb', 'json', 'json.erb')
+          end
+          .map { |file, info| info['logical_path'] }
+          .map { |file| [file, asset_path(file)] }
+      ]
+    else
+      assets = Hash[
+        Rails.application.assets.each_file
+          .select do |file|
+            file.include?(Rails.root.to_s) && file.end_with?('html', 'html.erb', 'json', 'json.erb')
+          end
+          .map { |file| Rails.application.assets.find_asset(file).logical_path }
+          .map { |file| [file, asset_path(file)] }
+      ]
+    end
 
     data = {
       env: Rails.env,
